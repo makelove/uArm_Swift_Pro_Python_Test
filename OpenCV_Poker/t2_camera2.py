997a303d48c0828ca4273d03eac5a9027da94c21
play4fun <play4fun@foxmail.com>
2017-07-21 12:35:38 +0800

    camera test

--- OpenCV_Poker/t2_camera2.py
+++ OpenCV_Poker/t2_camera2.py
@@ -24,24 +24,30 @@ from time import sleep
 arm_move = False
 
 logger_init(logging.INFO)
-print('setup swift ...')
-port = '/dev/' + [x for x in os.listdir('/dev/') if x.startswith('cu.usbmodem')][0]
-# port = '/dev/' + [x for x in os.listdir('/dev/') if x.startswith('ttyA')][0]
-# swift = SwiftAPI(dev_port='/dev/cu.usbmodem1421')
-print(port)
-swift = SwiftAPI(dev_port=port)
 
-sleep(2)
+def init_uarm():
 
-# 复位
-swift.set_position(x=60, y=0, z=50, speed=1800, wait=True)
-# swift.set_position(x=60, y=0, z=50, speed=1800, wait=False)
-# while swift.get_is_moving():
-#     sleep(0.1)
-swift.set_buzzer()
+    print('setup swift ...')
+    port = '/dev/' + [x for x in os.listdir('/dev/') if x.startswith('cu.usbmodem')][0]
+    # port = '/dev/' + [x for x in os.listdir('/dev/') if x.startswith('ttyA')][0]
+    # swift = SwiftAPI(dev_port='/dev/cu.usbmodem1421')
+    print(port)
+    swift = SwiftAPI(dev_port=port)
+
+    sleep(2)
+
+    # 复位
+    swift.set_position(x=60, y=0, z=50, speed=1800, wait=True)
+    # swift.set_position(x=60, y=0, z=50, speed=1800, wait=False)
+    # while swift.get_is_moving():
+    #     sleep(0.1)
+    swift.set_buzzer()
+    return swift
+# swift=init_uarm()
 
 
 def pick_place_poker(x, y):  #
+    global swift
 
     print('机械臂启动')
     swift.set_buzzer()
@@ -116,16 +122,17 @@ def draw_line_rectangle(frame):
     # 中间
     cv2.line(frame, (half, 0), (half, rows), (0, 0, 255), 5)
     # 左边
-    up_left = (int(cols / 2 * 1 / 8), int(rows * 1 / 8))  # 左上点
-    down_right = (int(cols / 2 * 7 / 8), int(rows * 7 / 8))  # 右下点
+    up_left1 = (int(cols / 2 * 1 / 8), int(rows * 1 / 8))  # 左上点
+    down_right1 = (int(cols / 2 * 7 / 8), int(rows * 7 / 8))  # 右下点
     # print(up_left, down_right)
-    cv2.rectangle(frame, up_left, down_right, (0, 255, 0), 3)
+    cv2.rectangle(frame, up_left1, down_right1, (0, 255, 0), 3)
 
     # 右边
-    up_left = (int(cols / 2 * 1 / 8 + cols / 2), int(rows * 1 / 8))  # 左上点
-    down_right = (int(cols / 2 * 7 / 8 + cols / 2), int(rows * 7 / 8))  # 右下点
+    up_left2 = (int(cols / 2 * 1 / 8 + cols / 2), int(rows * 1 / 8))  # 左上点
+    down_right2 = (int(cols / 2 * 7 / 8 + cols / 2), int(rows * 7 / 8))  # 右下点
     # print(up_left, down_right)
-    cv2.rectangle(frame, up_left, down_right, (255, 0, 0), 3)
+    cv2.rectangle(frame, up_left2, down_right2, (255, 0, 0), 3)
+    return up_left1,down_right1,up_left2,down_right2
 
 
 # while (True):
@@ -133,8 +140,17 @@ while (cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    # 划线
+    up_left1, down_right1, up_left2, down_right2=draw_line_rectangle(frame)
+
     # ROI
-    frame2 = frame[80:560, 90:630]
+    # frame2 = frame[80:560, 90:630]#
+    frame2 = frame[ up_left1[1]:down_right1[1],up_left1[0]:down_right1[0]]#
+    # print(up_left1[0],down_right1[0], up_left1[1],down_right1[1])
+    # cv2.imshow('frame2', frame2)
+
+    # cv2.waitKey(0)
+    # break
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
@@ -188,14 +204,15 @@ while (cap.isOpened()):
                     # pick_place_poker(move=True)
 
                     arm_move = True
-                    _thread.start_new_thread(move_thread, (mx, my))
+                    # _thread.start_new_thread(move_thread, (mx, my))
 
-    # 划线
-    draw_line_rectangle(frame)
+
 
     # Display the resulting frame
     cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('frame', frame)
+    cv2.imshow('frame2', frame2)
+
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
