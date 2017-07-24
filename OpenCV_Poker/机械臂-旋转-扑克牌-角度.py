e851a3a778f091e6c0ec9ad418aa0be49b3d6e09
play4fun <play4fun@foxmail.com>
2017-07-24 09:25:22 +0800

    气泵旋转多个角度

--- OpenCV_Poker/机械臂-旋转-扑克牌-角度.py
+++ OpenCV_Poker/机械臂-旋转-扑克牌-角度.py
@@ -33,13 +33,30 @@ swift.set_pump(on=True)
 #升起
 swift.set_position(230, 0, 70, speed=1500,wait=True)
 #旋转90度
-print('角度：',swift.get_servo_angle())#[90.0, 74.85, 39.1, 90.0]
+print('角度：',swift.get_servo_angle())#[90.09, 74.76, 39.27, 93.0]
 print('旋转90度')
-ret=swift.set_wrist(angle=90+30,wait=True)#默认是90度
+ret=swift.set_wrist(angle=90-90,wait=True)#默认是90度
+print('角度：',swift.get_servo_angle(),ret)#[90.09, 74.94, 39.01, 104.0]
+sleep(1)
+ret=swift.set_wrist(angle=90-60,wait=True)#
+print('角度：',swift.get_servo_angle(),ret)#[90.09, 75.03, 38.92, 15.0]
+sleep(1)
+ret=swift.set_wrist(angle=90-30,wait=True)#
+print('角度：',swift.get_servo_angle(),ret)#[90.09, 75.03, 38.92, 52.0]
+sleep(1)
+ret=swift.set_wrist(angle=90+0,wait=True)#
+print('角度：',swift.get_servo_angle(),ret)#[90.09, 75.03, 38.92, 65.0]
+sleep(1)
+ret=swift.set_wrist(angle=90+30,wait=True)#
+print('角度：',swift.get_servo_angle(),ret)#[90.09, 75.03, 38.92, 94.0]
+sleep(1)
+ret=swift.set_wrist(angle=90+60,wait=True)#
+print('角度：',swift.get_servo_angle(),ret)#[90.09, 75.03, 38.92, 118.0]
+sleep(1)
+ret=swift.set_wrist(angle=90+90,wait=True)#
 # ret=swift.set_servo_angle(servo_id=SERVO_HAND,angle=90+20,wait=True)
 # if ret is False:
-print('角度：',swift.get_servo_angle(),ret)#[90.0, 75.03, 38.83, 93.0]
-
+print('角度：',swift.get_servo_angle(),ret)#[90.09, 75.03, 38.92, 168.0]
 sleep(1)
 
 # 气泵
@@ -54,4 +71,4 @@ except KeyboardInterrupt as e:
     print('KeyboardInterrupt', e)
 finally:
     # print('ret5: ' + test_ports['swift_service']['handle'].call('set cmd_sync G0 X80 Y0 Z60'))
-    swift.set_position(x=100, y=0, z=10, speed=1800, wait=True)
+    swift.set_position(x=100, y=0, z=20, speed=1800, wait=True)
