619adaa4417c54f8ab77a44da6aa1b580847d649
play4fun <play4fun@foxmail.com>
2017-08-20 21:40:37 +0800

    解锁电机 2

--- 解锁电机.py
+++ 解锁电机.py
@@ -11,19 +11,41 @@ from time import sleep
 from uf.wrapper.swift_api import SwiftAPI
 from uf.utils.log import *
 
-#logger_init(logging.VERBOSE)
-#logger_init(logging.DEBUG)
+# logger_init(logging.VERBOSE)
+# logger_init(logging.DEBUG)
 logger_init(logging.INFO)
 
 print('setup swift ...')
 
-#swift = SwiftAPI(dev_port = '/dev/ttyACM0')
-#swift = SwiftAPI(filters = {'hwid': 'USB VID:PID=2341:0042'})
-swift = SwiftAPI() # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
-
+# swift = SwiftAPI(dev_port = '/dev/ttyACM0')
+# swift = SwiftAPI(filters = {'hwid': 'USB VID:PID=2341:0042'})
+swift = SwiftAPI()  # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
 
 print('sleep 2 sec ...')
 sleep(2)
 
 print('device info: ')
 print(swift.get_device_info())
+
+#
+print('开始测试')
+swift.set_buzzer()
+swift.set_buzzer()
+
+print('解锁电机')
+swift.set_servo_detach()
+sleep(10)
+swift.set_buzzer()#TODO 哔哔2声
+swift.set_buzzer()
+sleep(2)
+
+print('解锁电机')
+swift.set_servo_attach()
+sleep(3)
+
+print('重置机械臂')
+swift.set_buzzer()
+swift.reset(x=103,
+            y=0,
+            z=42,
+            speed=800)
