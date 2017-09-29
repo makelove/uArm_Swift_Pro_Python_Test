0fdca2c30127ea6a8b05b36378e1d85dd879433a
play4fun <play4fun@foxmail.com>
2017-09-30 00:31:44 +0800

    register_report_position_callback

--- test2/test_swift_api.py
+++ test2/test_swift_api.py
@@ -15,16 +15,23 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from uf.wrapper.swift_api import SwiftAPI
 from uf.utils.log import *
 
-#logger_init(logging.VERBOSE)
-#logger_init(logging.DEBUG)
+# logger_init(logging.VERBOSE)
+# logger_init(logging.DEBUG)
 logger_init(logging.INFO)
 
 print('setup swift ...')
 
-#swift = SwiftAPI(dev_port = '/dev/ttyACM0')
-#swift = SwiftAPI(filters = {'hwid': 'USB VID:PID=2341:0042'})
-swift = SwiftAPI() # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
+# swift = SwiftAPI(dev_port = '/dev/ttyACM0')
+# swift = SwiftAPI(filters = {'hwid': 'USB VID:PID=2341:0042'})
+swift = SwiftAPI()  # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
 
+
+def report_position(data):
+    print('data:', data)  # [x, y, z, r], r is wrist angle, ignore if not used
+
+
+swift.register_report_position_callback(report_position)
+swift.set_report_position(interval=0.5)  # 0.5 second (set 0 disable report)
 
 print('sleep 2 sec ...')
 sleep(2)
@@ -33,18 +40,18 @@ print('device info: ')
 print(swift.get_device_info())
 
 print('\nset X350 Y0 Z50 F500 ...')
-swift.set_position(350, 0, 50, speed = 1500)
+swift.set_position(350, 0, 50, speed=1500)
 
 print('set X340 ...')
-swift.set_position(x = 340)
+swift.set_position(x=340)
 print('set X320 ...')
-swift.set_position(x = 320)
+swift.set_position(x=320)
 print('set X300 ...')
-swift.set_position(x = 300)
+swift.set_position(x=300)
 print('set X200 ...')
-swift.set_position(x = 200)
+swift.set_position(x=200)
 print('set X190 ...')
-swift.set_position(x = 190)
+swift.set_position(x=190)
 
 # make sure at least one position was sent to device before start checking
 sleep(0.1)
@@ -54,9 +61,9 @@ while swift.get_is_moving():
     sleep(0.1)
 
 print('set Z100 ...')
-swift.set_position(z = 100, wait = True)
+swift.set_position(z=100, wait=True)
 print('set Z150 ...')
-swift.set_position(z = 150, wait = True)
+swift.set_position(z=150, wait=True)
 
 swift.set_buzzer()
 
@@ -69,4 +76,3 @@ except KeyboardInterrupt as e:
 finally:
     # print('ret5: ' + test_ports['swift_service']['handle'].call('set cmd_sync G0 X80 Y0 Z60'))
     swift.set_position(x=80, y=0, z=60, speed=1800, wait=True)
-
