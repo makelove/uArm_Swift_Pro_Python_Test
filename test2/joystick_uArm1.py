51568a03b988a363b925c387e6e6c9ce33a2ea34
play4fun <play4fun@foxmail.com>
2017-10-02 14:53:43 +0800

    使用游戏手柄控制uArm Swift pro机械臂

--- test2/joystick_uArm1.py
+++ test2/joystick_uArm1.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#使用游戏手柄控制uArm Swift pro机械臂
 import sys, os
 from time import sleep
 
@@ -24,8 +26,6 @@ swift = SwiftAPI()  # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
 
 # def report_position(data):
 #     print('data:', data)  # [x, y, z, r], r is wrist angle, ignore if not used
-#
-#
 # swift.register_report_position_callback(report_position)
 # swift.set_report_position(interval=0.5)  # 0.5 second (set 0 disable report)
 
@@ -47,36 +47,48 @@ try:
     for event in device.read_loop():
         cate = categorize(event)
         if event.type == ecodes.EV_KEY:
-            print('EV_KEY', cate, event)
+            print('EV_KEY',  event)
 
         if event.type == ecodes.EV_ABS:
-            print('EV_ABS', cate, event)
+            print('EV_ABS',  event)
         if event.value == 0:
             continue
-        if event.code == 0:# Not good
-            x1 = 5 if event.value < 0 else -5
-            pos = swift.get_position()
-            x = pos[0]
-            swift.set_position(x=x + x1)
-        if event.code == 1:
-            y1 = 5 if event.value < 0 else -5
-            pos = swift.get_position()
-            y = pos[1]
-            swift.set_position(y=y + y1)
 
-        # if event.code ==17:#good
+        # if event.code == 0:# Not good #左边摇杆
         #     x1 = 5 if event.value < 0 else -5
-        #     pos=swift.get_position()
-        #     x=pos[0]
-        #     swift.set_position(x=x+x1)
-        #
-        # if event.code ==16:
+        #     pos = swift.get_position()
+        #     x = pos[0]
+        #     swift.set_position(x=x + x1)
+        # if event.code == 1:
         #     y1 = 5 if event.value < 0 else -5
-        #     pos=swift.get_position()
+        #     pos = swift.get_position()
         #     y = pos[1]
-        #     swift.set_position(y=y+y1)
+        #     swift.set_position(y=y + y1)
 
-        if event.code == 315:
+        if event.code ==17:#good #左边，上和下
+            x1 = 5 if event.value < 0 else -5
+            pos=swift.get_position()
+            x=pos[0]
+            swift.set_position(x=x+x1)
+
+        if event.code ==16:#左边，左和右
+            y1 = 5 if event.value < 0 else -5
+            pos=swift.get_position()
+            y = pos[1]
+            swift.set_position(y=y+y1)
+        if event.code ==308:#右边，上-三角形
+            z = 35
+            swift.set_position(z=z)
+        if event.code ==304:#右边，下-X
+            z=2
+            swift.set_position(z=z)
+        if event.code == 307:#右边，左-正方形
+            swift.set_pump(on=True)
+        if event.code == 305:#右边，左-圆圈
+            swift.set_pump(on=False)
+
+
+        if event.code == 315:#Start键，退出
             print('break')
             break
             # while True:
@@ -85,4 +97,5 @@ except KeyboardInterrupt as e:
     print('KeyboardInterrupt', e)
 finally:
     # print('ret5: ' + test_ports['swift_service']['handle'].call('set cmd_sync G0 X80 Y0 Z60'))
+    swift.set_pump(on=False)
     swift.set_position(x=80, y=0, z=30, speed=1800, wait=True)
